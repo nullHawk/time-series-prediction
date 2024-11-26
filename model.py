78eef4db33a3d3ddecab541e91327ee4f592e227
Suryansh <suryanshsinghshakya1@gmail.com>
2024-11-26 11:13:20 +0000

    add: model.py

--- model.py
+++ model.py
@@ -1,6 +1,41 @@
 import torch
 import torch.nn as nn
 
-class SequenceModel(nn.Module):
-    def __init__(self):
-        super(SequenceModel, self).__init__()
+class LSTMPredictor(nn.Module):
+    def __init__(self, n_hidden=51):
+        super(LSTMPredictor, self).__init__()
+        self.hidden = n_hidden
+        self.lstm1 = nn.LSTMCell(1, self.n_hidden)
+        self.lstm2 = nn.LSTMCell(self.n_hidden, self.n_hidden)
+        self.linear = nn.Linear(self.n_hidden, 1)
+    
+    def forward(self, x, future = 0):
+        outputs = []
+        n_samples = x.size(0)
+
+        h_t = torch.zeros(n_samples, self.n_hidden, dtype=torch.float32)
+        c_t = torch.zeros(n_samples, self.n_hidden, dtype=torch.float32)
+        h_t2 = torch.zeros(n_samples, self.n_hidden, dtype=torch.float32)
+        c_t2 = torch.zeros(n_samples, self.n_hidden, dtype=torch.float32)
+
+        for input_t in x.split(1, dim=1): # split the tensor into chunks and each chunk is view of the orignal tensor
+            # N, 1
+            h_t, c_t = self.lstm1(input_t, (h_t, c_t))
+            h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
+            output = self.linear(h_t2)
+            outputs.append(output)
+        
+        for i in range(future):
+            h_t, c_t = self.lstm1(output, (h_t, c_t))
+            h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
+            output = self.linear(h_t2)
+            outputs.append(output)
+
+        outputs = torch.cat(outputs, dim=1)
+        return outputs
+
+# if __name__ == '__main__':
+#     train_input = torch.from_numpy(y[3:,:-1])
+#     train_target = torch.from_numpy(y[3:, 1:])
+#     model = LSTMPredictor()
+#     print(model)
