099877b92b3d3a76f9e5f86e3281a70dcfd28821
Suryansh <suryanshsinghshakya1@gmail.com>
2024-11-26 11:21:20 +0000

    update: generate_sinewave.py

--- generate_sinewave.py
+++ generate_sinewave.py
@@ -4,21 +4,21 @@ import torch.nn as nn
 import torch.optim as optim
 import matplotlib.pyplot as plt
 
-N = 100 # number of samples
-L = 1000 # length of sequence
-T = 20 # period of sin wave
 
-x = np.empty((N, L), np.float32)
-x[:] = np.array(range(L)) + np.random.randint(-4*T, 4*T, N).reshape(N, 1) # shift the wave randomly
-y = np.sin(x/1.0/T).astype(np.float32) 
+def generate(N=100, L=1000, T=20):
+    x = np.empty((N, L), np.float32)
+    x[:] = np.array(range(L)) + np.random.randint(-4*T, 4*T, N).reshape(N, 1) # shift the wave randomly
+    y = np.sin(x/1.0/T).astype(np.float32) 
+    return x, y
+
+def plot(x, y):
+    plt.figure(figsize=(10, 8))
+    plt.title('Sin wave')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.xticks(fontsize=20)
+    plt.yticks(fontsize=20)
+    plt.plot(np.arange(x.shape[1]), y[0,:], 'r', linewidth=2.0)
+    plt.show()
 
 
-
-plt.figure(figsize=(10, 8))
-plt.title('Sin wave')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.xticks(fontsize=20)
-plt.yticks(fontsize=20)
-plt.plot(np.arange(x.shape[1]), y[0,:], 'r', linewidth=2.0)
-plt.show()
---
+++ train.py
@@ -0,0 +1,7 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import matplotlib.pyplot as plt
+from model import LSTMPredictor
+import generate_sinewave
